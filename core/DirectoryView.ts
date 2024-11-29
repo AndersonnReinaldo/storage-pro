b749cd7bfaec083fcf048d07215667f18237d983
REINALDO <andersonnreinaldo@gmail.com>
2024-11-29 14:21:39 -0300

    first commit

---
+++ core/DirectoryView.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import { Complex, stripPath } from "../utils.js";
+
+export class DirectoryView {
+  static listFromPath(_path: string): Complex {
+    const stat = fs.statSync(_path);
+
+    if (stat.isDirectory()) {
+      const files = fs.readdirSync(_path);
+
+      const tree = files.map((file) => {
+        const newPath = path.join(_path, file);
+        return DirectoryView.listFromPath(newPath);
+      });
+
+      return {
+        type: "folder",
+        name: path.basename(_path),
+        path: stripPath(_path),
+        content: tree,
+      };
+    } else {
+      return {
+        type: "file",
+        path: stripPath(_path),
+        name: path.basename(_path),
+        datetime: stat.mtime,
+        size: stat.size,
+      };
+    }
+  }
+}
---
+++ core/FileProcessor.ts
@@ -0,0 +1,124 @@
+import path from "path";
+import multer from "multer";
+import { _root, wwwroot } from "../utils.js";
+import { RequestFile } from "./RequestFile.js";
+import sharp from "sharp";
+import fs from "fs";
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+  "image/png": "png",
+  "application/pdf": "pdf",
+  "video/mp4": "mp4",
+  "audio/mp3": "mp3",
+  "application/javascript": "js",
+  "application/json": "json",
+  "application/xml": "xml",
+  "text/plain": "txt",
+  "text/html": "html",
+  "text/css": "css",
+  "text/javascript": "js",
+};
+
+const methodHandler = {
+    POST: async (req: any): Promise<void> => {
+        const request = {
+            projectName: req.query.projectName,
+            projectScope: req.query.projectScope,
+        };
+    }, 
+    PUT: async  (req: any): Promise<void>=> {
+        const request = {
+            projectName: req.query.projectName,
+            projectScope: req.query.projectScope,
+            oldFileName: req.query.oldFileName
+        };
+
+        if (request.oldFileName) {
+            console.log(request.oldFileName);
+            await RequestFile.deleteFile(path.join(wwwroot, request.projectName, request.projectScope, request.oldFileName));
+        }
+    },
+}
+
+const storage = multer.diskStorage({
+  destination: async (req, file, callback) => {
+    let path;
+
+    try {
+      //@ts-ignore
+      await methodHandler[req.method](req);
+      const prepare = await RequestFile.preparePath((req.query as any).projectName, (req.query as any).projectScope);
+      path = prepare.path;
+    } catch (error) {
+      req._destroy(error as Error, d => {
+        console.log(d);
+      })
+
+      console.log(error)
+    }
+
+    callback(null, path!);
+
+  },
+  filename: (req, file, callback) => {
+    const lastDot = file.originalname.lastIndexOf(".");
+    const name = file.originalname.slice(0, lastDot).split(" ").join("_");
+    const extensionFile = file.originalname.slice(lastDot + 1);
+    const extension = MIME_TYPES[file.mimetype as keyof typeof MIME_TYPES] || 
+                      (Object.values(MIME_TYPES).includes(extensionFile) 
+                              ? extensionFile 
+                              : "txt");
+
+    console.log(lastDot, name, extensionFile, extension);
+    callback(null, `${name}.${extension}`);
+  },
+});
+
+export const FileProcessor = multer({ storage: storage, fileFilter: (req, file, cb) => {
+  // if (!req.query.projectName) {
+  //   return cb(new Error("Invalid projectName"));
+  // }
+
+  // temporary disable filters
+
+  // if (!RequestFile.validJoin(req.query.projectName, req.query.projectScope)) {
+  //   return cb(new Error("Invalid path! wwwroot is a reserved path"));
+  // }
+
+  cb(null, true);
+} }).array("file", 5);
+
+
+export const resizeImage = async (req: any, res: any, next: any) => {
+  if (!req.files) {
+    return next();
+  }
+
+  try {
+    await Promise.all(req.files.map(async (file: any) => {
+      const filePath = path.join(file.destination, file.filename);
+      const outputFilePath = path.join(file.destination, `resized-${file.filename}`);
+      await sharp(filePath)
+        .resize(800, 800, {
+          fit: sharp.fit.inside,
+          withoutEnlargement: true,
+        })
+        .toFormat('webp') // Forçamos a conversão esse arquivo para webp
+  
+        .webp({ // Comprimimos, setando uma qualidade
+            quality: 80
+        })
+        .toFile(outputFilePath)
+
+
+      fs.unlinkSync(filePath);
+      fs.renameSync(outputFilePath, filePath);
+    }));
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
